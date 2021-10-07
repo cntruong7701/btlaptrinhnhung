2a94321db8933a80710c41bd2401f36e5e916cc2
your name <cntruong12c5nct@gmail.com>
2021-10-07 22:33:14 +0700

    btlaptrinhnhung

---
+++ baitap5/led7doan.ino
@@ -0,0 +1,17 @@
+
+int a = 6, b =5, c = 4, d = 3, e = 2, f = 1, g = 0;
+void setup() {
+  pinMode(a,OUTPUT); pinMode(b,OUTPUT); pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT); pinMode(e,OUTPUT); pinMode(g,OUTPUT);
+  pinMode(g,OUTPUT);
+}
+void KHONG() {
+  digitalWrite(a,HIGH); digitalWrite(b,HIGH); digitalWrite(c,LOW);
+  digitalWrite(d,HIGH); digitalWrite(e,LOW); digitalWrite(f,HIGH);
+  digitalWrite(g,LOW);
+  }
+
+void MOT() {
+  }
+void loop() {
+}
