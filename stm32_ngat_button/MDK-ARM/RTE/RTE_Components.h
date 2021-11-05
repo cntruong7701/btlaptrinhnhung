ce3b7a0f12d51abf099b34bd62320fcdac006d71
your name <cntruong12c5nct@gmail.com>
2021-11-05 09:18:52 +0700

    btlaptrinhnhung

---
+++ stm32_ngat_button/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32_ngat_button' 
+ * Target:  'stm32_ngat_button' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ stm32_ngat_button/MDK-ARM/stm32_ngat_button/startup_stm32f401xe.d
@@ -0,0 +1,1 @@
+stm32_ngat_button\startup_stm32f401xe.o: startup_stm32f401xe.s
